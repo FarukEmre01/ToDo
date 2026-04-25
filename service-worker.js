65de85f0d9b354c1957fdd50e1873da22e3c6991
FarukEmre01 <farukemrekara01@gmail.com>
2026-04-26 01:13:21 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'tickoro-v2';
+const CACHE_NAME = 'tickoro-v3';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
@@ -6,8 +6,7 @@ const ASSETS_TO_CACHE = [
   './script.js',
   './drag-drop-touch.js',
   './manifest.json',
-  './icons/icon-192.png',
-  './icons/icon-512.png'
+  './icons/icon.jpg'
 ];
 
 self.addEventListener('install', (event) => {
